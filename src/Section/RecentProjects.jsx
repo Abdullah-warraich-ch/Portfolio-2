a59907b9d24b5b71cad67116a5bd0023245e21fc
Abdullah-warraich-ch <abdullahnasar333@gmail.com>
2026-01-20 17:37:14 +0500

    Project Updated

--- src/Section/RecentProjects.jsx
+++ src/Section/RecentProjects.jsx
@@ -10,11 +10,13 @@ function RecentProjects() {
       <div className="mb-10">
         <Heading FWord="RECENT" LWord="PROJECTS" />
       </div>
-      <RecentProjectCard
-        pic="https://cdn.dribbble.com/userupload/2646172/file/original-4dfec15f2df0816cc270d85c1f340288.jpg?resize=400x0"
-        heading="Tutor Booking"
-        discr="Book A tutor"
-      />
+      <a href="https://e-commerce-product-page-beta-sand.vercel.app/">
+        <RecentProjectCard
+          pic="project1.png"
+          heading="Simple E-Commerce Page"
+          discr="Product Listing + Add to Cart"
+        />
+      </a>
 
       <RecentProjectCard
         pic="https://cdn.sanity.io/images/599r6htc/regionalized/1f5848c2e38f2b9c8a5563e4f672c60e8ae97b57-2400x2400.png?w=2400&h=2400&q=75&fit=max&auto=format"
